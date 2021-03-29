1acc351c4e995a8afdaf8988ed73ece05fc75035
P1nk64's p775tm <yx.chn@foxmail.com>
2021-03-30 00:13:50 +0800

    这是我第四次提交文件

---
+++ src/com/gittest/ThirdFile.java
@@ -0,0 +1,37 @@
+package com.gittest;
+
+public class ThirdFile {
+    private int id;
+    private String name;
+    private Boolean sex;//man is true
+
+    public ThirdFile(int id, String name, Boolean sex) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+}
