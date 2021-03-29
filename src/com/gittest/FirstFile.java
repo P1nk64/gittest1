116a24c45fd93b00406397e118054a0c74c0184d
P1nk64's p775tm <yx.chn@foxmail.com>
2021-03-30 00:03:22 +0800

    这是我第三次提交文件

--- src/com/gittest/FirstFile.java
+++ src/com/gittest/FirstFile.java
@@ -5,5 +5,10 @@ public class FirstFile {
         System.out.println("Hello World");
         SecondFile sf = new SecondFile();
         sf.ShowMe("这是ShowMe函数的方法");
+        ThirdFile tf = new ThirdFile(01,"yx",true);
+        System.out.println(tf.getId());
+        System.out.println(tf.getName());
+        tf.setName("Yang Xiao");
+        System.out.println(tf.getName());
     }
 }
