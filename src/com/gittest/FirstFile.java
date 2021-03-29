ca9e701a556d8020073eba2cd56e7ceb747b8fb9
P1nk64's p775tm <yx.chn@foxmail.com>
2021-03-29 23:56:24 +0800

    这是我第二次提交文件

--- src/com/gittest/FirstFile.java
+++ src/com/gittest/FirstFile.java
@@ -3,5 +3,7 @@ package com.gittest;
 public class FirstFile {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        SecondFile sf = new SecondFile();
+        sf.ShowMe("这是ShowMe函数的方法");
     }
 }
---
+++ src/com/gittest/SecondFile.java
@@ -0,0 +1,12 @@
+package com.gittest;
+
+public class SecondFile {
+    public SecondFile() {
+        System.out.println("这是第二个文件的构造方法");
+    }
+
+    public void ShowMe(String str){
+        System.out.println(str);
+    }
+
+}
